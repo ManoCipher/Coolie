8f13653ebd5e93bdbad3e35451e762f32244bc00
ManoCipher <mano720051@gmail.com>
2025-10-24 22:38:24 +0530

    Initial Coolie backend setup

---
+++ src/test/java/com/coolie/coolie_backend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolie.coolie_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
